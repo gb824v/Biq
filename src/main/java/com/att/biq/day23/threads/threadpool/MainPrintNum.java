c7fd4eec147efb1e69c33afdf2b80d7b74bd3d1d
gb824v <gb824v@intl.att.com>
2018-04-22 18:07:30 +0300

    AnalyzeInputs updates

--- src/main/java/com/att/biq/day23/threads/threadpool/MainPrintNum.java
+++ src/main/java/com/att/biq/day23/threads/threadpool/MainPrintNum.java
@@ -3,24 +3,31 @@ package com.att.biq.day23.threads.threadpool;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MainPrintNum {
+public class MainPrintNum
+{
 	private static int numOfTasks = 5;
 	private static int numOfThreads = 5;
 
-	static public synchronized void printNumbers() {
-		for (int i = 0; i < numOfTasks; i++) {
+	static public synchronized void printNumbers()
+	{
+		for (int i = 0; i < numOfTasks; i++)
+		{
 			System.out.println("ThreadId:" + Thread.currentThread().getId() + " Session:" + i);
 		}
 	}
 
-	static public void main(String[] args) {
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		for (int i = 0; i < numOfThreads; i++) {
+	static public void main(String[] args)
+	{
+		ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
+		
+		for (int i = 0; i < numOfTasks; i++)
+		{
 			Worker worker = new Worker();
 			executor.execute(worker);
 		}
 		executor.shutdown(); // When all workers finished, shutdown
-		while (!executor.isTerminated()) {
+		while (!executor.isTerminated())
+		{
 		}
 		System.out.println("Done !!!");
 	}
