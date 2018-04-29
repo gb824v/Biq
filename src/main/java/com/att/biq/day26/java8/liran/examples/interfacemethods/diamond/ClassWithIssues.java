68e83a6bd67914d1055fb657c562c976424ce875
gb824v <gb824v@intl.att.com>
2018-04-29 11:45:50 +0300

    [US00001] Refactoring according to lattice reportportal

---
+++ src/main/java/com/att/biq/day26/java8/liran/examples/interfacemethods/diamond/ClassWithIssues.java
@@ -0,0 +1,5 @@
+package com.att.biq.day26.java8.liran.examples.interfacemethods.diamond;
+
+//public class ClassWithIssues implements InterfaceA, InterfaceB{
+
+//}
---
+++ src/main/java/com/att/biq/day26/java8/liran/examples/interfacemethods/diamond/InterfaceA.java
@@ -0,0 +1,7 @@
+package com.att.biq.day26.java8.liran.examples.interfacemethods.diamond;
+
+public interface InterfaceA {
+    default int calc(int x1, int x2) {
+        return x1 + x2;
+    }
+}
